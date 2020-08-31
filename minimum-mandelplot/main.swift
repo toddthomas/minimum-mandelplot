7216fe9aa740247331595cc87de25cee73086978
Todd Thomas <toddthomas@acm.org>
2020-08-31 12:35:44 -0600

    Initial Commit

---
+++ minimum-mandelplot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  minimum-mandelplot
+//
+//  Created by Todd Thomas on 8/31/20.
+//  Copyright © 2020 Todd Thomas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
